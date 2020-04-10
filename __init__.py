288738b88bde9649970fe81a5067ce2d032d18e7
syeraazizi <63461580+syeraazizi@users.noreply.github.com>
2020-04-11 01:14:35 +0800

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,15 +1,15 @@
-#!/usr/bin/env python
-import pip._internal.utils.inject_securetransport  # noqa
 from pip._internal.utils.typing import MYPY_CHECK_RUNNING
 
 if MYPY_CHECK_RUNNING:
-    from typing import Optional, List
+    from typing import List, Optional
+
+
+__version__ = "20.0.2"
 
 
 def main(args=None):
     # type: (Optional[List[str]]) -> int
-    """This is preserved for old console scripts that may still be referencing
-    it.
+    """This is an internal API only meant for use by pip's own console scripts.
 
     For additional details, see https://github.com/pypa/pip/issues/7498.
     """
